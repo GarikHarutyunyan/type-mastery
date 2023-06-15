a345b5c62aad794c986741ca4a3592b3d9af5891
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2023-06-15 20:13:39 +0400

    add highlighting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import React, { useState, FC } from 'react';
-import { useKeyboard } from './hooks/useKeyboard';
+import React, { FC } from 'react';
 
 import './App.css';
+import { Main } from './pages/main/Main';
 
 const App: FC = () => {
-  const [pressedKey, setPressedKey] = useState<string>("");
-
-  useKeyboard(setPressedKey);
 
   return (
     <div className="App">
-      <h1>{pressedKey}</h1>
+      <Main />
     </div>
   );
 }
---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,28 @@
+import React, { FC, useState } from 'react';
+import { useKeyboard } from '../../hooks/useKeyboard';
+
+// import styles from "../../styles/Main.module.css";
+
+export const Main: FC = () => {
+    const [pressedKey, setPressedKey] = useState<string>("");
+    const [plainText] = useState<string>("aaa");
+    // const [inputString, setInputString] = useState<string>("");
+
+    useKeyboard(setPressedKey);
+
+    return (
+        <div>
+            <h1>{plainText}</h1>
+            <h1>
+                {plainText.split('').map((char, index) => (
+                    <span
+                        key={index}
+                        style={{ color: plainText.charAt(index) === pressedKey ? 'green' : 'black' }}
+                    >
+                        {char}
+                    </span>
+                ))}
+            </h1>
+        </div>
+    )
+}
