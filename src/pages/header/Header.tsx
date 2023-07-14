8a6d50c5707163f60b48aafd62df34423e13f055
vaahe <vahebarseghyan9999@gmail.com>
2023-07-15 01:48:41 +0400

    add routes to header

--- src/pages/header/Header.tsx
+++ src/pages/header/Header.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import '../../styles/Header.module.css';
+import React from "react";
+import "../../styles/Header.module.css";
+import { Link } from "react-router-dom";
+
+const logo = "/typing.png";
 
 export const Header = () => {
-    return (
-        <header>Header</header>
-    )
-}
+  return (
+    <header>
+      <nav>
+        <ul>
+          <img src={logo} alt="logo" />
+          <div className="container">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/">About Us</Link>
+            </li>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </header>
+  );
+};
