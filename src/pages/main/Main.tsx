d29cfa57ffa7aeaba1c836b288f04f01e17470c9
vahe <vahebarseghyan9999@gmail.com>
2023-07-30 03:17:22 +0400

    patch modal's logic

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,126 +1,142 @@
 import clsx from 'clsx';
-import React, {useEffect, useRef, useState} from 'react';
+import { FC, useEffect, useRef, useState, MouseEvent } from 'react';
 
 import styles from './Main.module.css';
-import {useInputText} from '../../hooks/useInputText';
+import { Modal } from '../../components/modal/Modal';
+import { useInputText } from '../../hooks/useInputText';
 
 interface IMove {
-  time: number;
-  position: number;
+    time: number;
+    position: number;
 }
 
-export const Main: React.FC = () => {
-  const startTime = useRef<number>(0);
-  const [inputText, setPressedKey] = useState<string>('');
-  const [currentMoves, setCurrentMoves] = useState<IMove[]>([]);
-  const [previousMoves, setPreviousMoves] = useState<IMove[]>([]);
-  const [isReadyToStart, setIsReadyToStart] = useState<boolean>(true);
-  const currentTimeout = useRef<NodeJS.Timeout | undefined>(undefined);
-  const [previousCursorPosition, setPreviousCursorPosition] = useState<
-    number | null
-  >(null);
+export const Main: FC = () => {
+    const startTime = useRef<number>(0);
+    const [inputText, setPressedKey] = useState<string>('');
+    const [currentMoves, setCurrentMoves] = useState<IMove[]>([]);
+    const [previousMoves, setPreviousMoves] = useState<IMove[]>([]);
+    const [isReadyToStart, setIsReadyToStart] = useState<boolean>(true);
+    const currentTimeout = useRef<NodeJS.Timeout | undefined>(undefined);
+    const [previousCursorPosition, setPreviousCursorPosition] = useState<
+        number | null
+    >(null);
+    const [showModal, setShowModal] = useState<boolean>(false);
 
-  const initialText = `Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.`;
-  const initialSplittedText: string[] = initialText.split('');
+    const initialText = `Contrary to popular belief`;
+    const initialSplittedText: string[] = initialText.split('');
 
-  const {clearData} = useInputText(setPressedKey);
+    const { clearData } = useInputText(setPressedKey);
 
-  useEffect(() => {
-    if (!isReadyToStart) {
-      showNextMove();
-    }
-  }, [isReadyToStart]);
-
-  useEffect(() => {
-    if (isReadyToStart && inputText.length === 1) {
-      startTime.current = Date.now();
-      setCurrentMoves([{time: 0, position: 1}]);
-      setIsReadyToStart(false);
-    } else if (startTime.current) {
-      const currentTime: number = Date.now();
-      const time: number = currentTime - startTime.current;
-      const position: number = inputText.length;
-      const newMove: IMove = {time, position};
-
-      startTime.current = currentTime;
-      setCurrentMoves((state) => [...state, newMove]);
-    }
-  }, [inputText.length]);
-
-  const showNextMove = (): void => {
-    const currentMove: IMove = previousMoves[0];
-    if (currentMove) {
-      const {time, position} = currentMove;
-      currentTimeout.current = setTimeout(() => {
+    useEffect(() => {
         if (!isReadyToStart) {
-          setPreviousCursorPosition(position);
-          previousMoves.shift();
-          showNextMove();
+            showNextMove();
         }
-      }, time);
-    }
-  };
+    }, [isReadyToStart]);
 
-  const onRestart = (): void => {
-    setIsReadyToStart(true);
-    setPressedKey('');
-    clearData();
-    setPreviousMoves(currentMoves);
-    setPreviousCursorPosition(null);
-    setCurrentMoves([]);
-    startTime.current = 0;
+    useEffect(() => {
+        if (isReadyToStart && inputText.length === 1) {
+            startTime.current = Date.now();
+            setCurrentMoves([{ time: 0, position: 1 }]);
+            setIsReadyToStart(false);
+        } else if (startTime.current) {
+            const currentTime: number = Date.now();
+            const time: number = currentTime - startTime.current;
+            const position: number = inputText.length;
+            const newMove: IMove = { time, position };
 
-    if (currentTimeout) {
-      clearTimeout(currentTimeout.current);
-    }
-  };
+            startTime.current = currentTime;
+            setCurrentMoves((state) => [...state, newMove]);
+        }
+    }, [inputText.length]);
 
-  return (
-    <div className={styles.container}>
-      <button onClick={onRestart} style={{height: '50px', width: '200px'}}>
-        {'Restart'}
-      </button>
-      <div>
-        {initialSplittedText.map((letter, index) => {
-          const isCorrect: boolean = letter === inputText[index];
-          const isIncorrect: boolean = !isCorrect && index < inputText.length;
-          const isLastTyppedLetter: boolean = index === inputText.length;
-          const isPreviousCursorPosition: boolean =
-            index === previousCursorPosition;
+    const showNextMove = (): void => {
+        const currentMove: IMove = previousMoves[0];
+        if (currentMove) {
+            const { time, position } = currentMove;
+            currentTimeout.current = setTimeout(() => {
+                if (!isReadyToStart) {
+                    setPreviousCursorPosition(position);
+                    previousMoves.shift();
+                    showNextMove();
+                }
+            }, time);
+        }
+    };
 
-          return (
-            <>
-              {isPreviousCursorPosition && (
-                <span
-                  className={clsx(
-                    styles.textCursor,
-                    styles.previousTextCursor,
-                    styles.blink
-                  )}
-                >
-                  {'|'}
-                </span>
-              )}
-              {isLastTyppedLetter && (
-                <span className={clsx(styles.textCursor, styles.blink)}>
-                  {'|'}
-                </span>
-              )}
-              <span
-                key={`${letter}_${index}`}
-                className={clsx(
-                  styles.letter,
-                  {[styles.letter_space]: letter === ' '},
-                  {[styles.correct]: isCorrect},
-                  {[styles.incorrect]: isIncorrect}
-                )}
-              >
-                {letter}
-              </span>
-            </>
-          );
-        })}
-      </div>
-    </div>
-  );
+    const onRestart = (): void => {
+        setIsReadyToStart(true);
+        setPressedKey('');
+        clearData();
+        setPreviousMoves(currentMoves);
+        setPreviousCursorPosition(null);
+        setCurrentMoves([]);
+        startTime.current = 0;
+
+        if (currentTimeout) {
+            clearTimeout(currentTimeout.current);
+        }
+    };
+
+    const handleCloseModal = (e: MouseEvent<HTMLButtonElement>): void => {
+        e.preventDefault();
+        setShowModal(false);
+    };
+
+    useEffect(() => {
+        if (inputText.length === initialText.length) {
+            setShowModal(true);
+        }
+    }, [inputText.length]);
+
+    return (
+        <div className={styles.container}>
+            {showModal && <Modal isVisible={showModal} title='Your results' info='erwe' onClose={handleCloseModal} />}
+
+            <button onClick={onRestart} style={{ height: '50px', width: '200px' }}>
+                {'Restart'}
+            </button>
+
+            <div>
+                {initialSplittedText.map((letter, index) => {
+                    const isCorrect: boolean = letter === inputText[index];
+                    const isIncorrect: boolean = !isCorrect && index < inputText.length;
+                    const isLastTyppedLetter: boolean = index === inputText.length;
+                    const isPreviousCursorPosition: boolean =
+                        index === previousCursorPosition;
+
+                    return (
+                        <>
+                            {isPreviousCursorPosition && (
+                                <span
+                                    className={clsx(
+                                        styles.textCursor,
+                                        styles.previousTextCursor,
+                                        styles.blink
+                                    )}
+                                >
+                                    {'|'}
+                                </span>
+                            )}
+                            {isLastTyppedLetter && (
+                                <span className={clsx(styles.textCursor, styles.blink)}>
+                                    {'|'}
+                                </span>
+                            )}
+                            <span
+                                key={`${letter}_${index}`}
+                                className={clsx(
+                                    styles.letter,
+                                    { [styles.letter_space]: letter === ' ' },
+                                    { [styles.correct]: isCorrect },
+                                    { [styles.incorrect]: isIncorrect }
+                                )}
+                            >
+                                {letter}
+                            </span>
+                        </>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
