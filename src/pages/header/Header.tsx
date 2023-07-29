3c7f1647fae9319120c878c3ac7e38136e609b4f
vahe <vahebarseghyan9999@gmail.com>
2023-07-30 03:16:16 +0400

    clear header odd code

--- src/pages/header/Header.tsx
+++ src/pages/header/Header.tsx
@@ -1,49 +1,30 @@
-import {useState} from 'react';
-import {Link} from 'react-router-dom';
-import {Modal} from '../../components/Modal';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Header.module.css';
 
 const logo = '/typing.png';
 
 export const Header = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = (): void => {
-    setIsModalVisible(true);
-  };
-
-  const closeModal = (): void => {
-    setIsModalVisible(false);
-  };
-
   return (
-    <>
-      <header>
-        <nav>
-          <ul>
-            <img src={logo} alt="logo" />
-            <div className="container">
-              <li>
-                <Link to="/" onClick={showModal}>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/">About Us</Link>
-              </li>
-              <li>
-                <Link to="/">Login</Link>
-              </li>
-            </div>
-          </ul>
-        </nav>
-      </header>
-      <Modal
-        title={'Home page modal'}
-        info={'You are already in home page.You are already in home page.'}
-        isVisible={isModalVisible}
-        onClose={closeModal}
-      ></Modal>
-    </>
+    <header>
+      <nav>
+        <ul>
+          <img src={logo} alt="logo" />
+          <div className="container">
+            <li>
+              <Link to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/">About Us</Link>
+            </li>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </header>
   );
 };
