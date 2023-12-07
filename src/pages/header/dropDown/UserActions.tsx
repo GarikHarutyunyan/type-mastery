e5f2cd7a26b5285503f6e05a41824ab414f7569b
Vahe Barseghyan <vahebarseghyan9999@gmail.com>
2023-12-07 16:04:47 +0400

    modify header

---
+++ src/pages/header/dropDown/UserActions.tsx
@@ -0,0 +1,44 @@
+// import React from 'react';
+
+// export const UserActions = () => {
+//   return (
+//     <Box sx={{flexGrow: 0}}>
+//       <Tooltip title="Open settings">
+//         <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+//           <Avatar alt="Remy Sharp" src="/assets/avatar.svg" />
+//         </IconButton>
+//       </Tooltip>
+//       <Menu
+//         sx={{mt: '45px'}}
+//         id="menu-appbar"
+//         anchorEl={anchorElUser}
+//         anchorOrigin={{
+//           vertical: 'top',
+//           horizontal: 'right',
+//         }}
+//         keepMounted
+//         transformOrigin={{
+//           vertical: 'top',
+//           horizontal: 'right',
+//         }}
+//         open={Boolean(anchorElUser)}
+//         onClose={handleCloseUserMenu}
+//       >
+//         {settings.map((setting) => (
+//           <MenuItem key={setting} onClick={handleCloseUserMenu}>
+//             <Typography textAlign="center">{setting}</Typography>
+//           </MenuItem>
+//         ))}
+//       </Menu>
+//     </Box>
+//   );
+// };
+
+
+import React from 'react'
+
+export const UserActions = () => {
+  return (
+    <div>UserActions</div>
+  )
+}
